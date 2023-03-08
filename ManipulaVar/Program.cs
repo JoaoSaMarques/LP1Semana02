491674546173293054e3ca08bca48b89f4eddafb
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-08 17:59:59 +0000

    Exe1

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main()
+        {
+            string val;
+            int res;
+
+            //Tells user to input a number
+            Console.WriteLine("Input a number: ");
+            //User input
+            val = Console.ReadLine();
+            //Convert string into int
+            res = Convert.ToInt32(val);
+            //This line writes a sentence and then does -- to input
+            Console.WriteLine("-- behind value: ", -- res);
+            //This line writes a sentence and then does ++ to input
+            Console.WriteLine("++ behind value_ ", ++ res);
+
+        }
+    }
+}
+
