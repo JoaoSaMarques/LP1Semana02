5969f143975617aaf58e71cc6bfe8e6c64df2cad
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-10 12:23:45 +0000

    Exe10

---
+++ Triangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input a number: ");
+            string val = Console.ReadLine();
+           
+
+            for (int res= Convert.ToInt32(val);
+            res > res + 2; res++)
+                Console.WriteLine("x"); 
+        }
+    }
+}
