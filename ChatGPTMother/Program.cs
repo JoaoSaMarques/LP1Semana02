336f8e9decb23efca6d2ba783a01de10a23eaf82
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-10 12:23:33 +0000

    Exe9

--- ChatGPTMother/Program.cs
+++ ChatGPTMother/Program.cs
@@ -6,11 +6,14 @@ namespace ChatGPTMother
     {
         static void Main(string[] args)
         {
+            //Tell you to ask questions
             Console.WriteLine("Ask any questions you may have.");
+            //Read input
             string Option = Console.ReadLine();
+            //While not exit
             while (Option != "Exit")
             {
-                
+                //Available questions with answers
                 if (Option == "Are you a robot?")
                     Console.WriteLine("No, I am human.");
                 else if (Option == "Tea or Coffee")
@@ -20,6 +23,7 @@ namespace ChatGPTMother
                 else if (Option == "Cats or dogs")
                     Console.WriteLine("Cats");
 
+                //Continue
                 Option = Console.ReadLine();
                 
             }  
