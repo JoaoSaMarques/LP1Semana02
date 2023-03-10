6553829d6cdf901262946c36732fee616fada3b9
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-10 10:16:52 +0000

    Exe6

---
+++ Specials/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Need float for infinity
+            float infinityF = 0;
+
+            //Makes infinity positive
+            float positive = 1 / infinityF;
+            //Writes ∞
+            Console.WriteLine(positive);
+            //Makes infinity negative
+            float negative = -1 / infinityF;
+            //Writes -∞
+            Console.WriteLine(negative);
+
+            //Need double to tell whether infinity is true for + / -
+            double positiveD = double.PositiveInfinity;
+            double negativeD = double.NegativeInfinity;
+
+            //Negative infinity = -∞
+            Console.WriteLine(negativeD == double.NegativeInfinity);
+            //Positive Infinity = ∞
+            Console.WriteLine(positiveD == double.PositiveInfinity);
+        }
+    }
+}
