ea8073f2607eefd87f0bf885e530c9875cfaa3ae
joao sa marques <joao.sa.marques@hotmail.com>
2023-03-10 12:02:14 +0000

    Exe9

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ask any questions you may have.");
+            string Option = Console.ReadLine();
+            while (Option != "Exit")
+            {
+                
+                if (Option == "Are you a robot?")
+                    Console.WriteLine("No, I am human.");
+                else if (Option == "Tea or Coffee")
+                    Console.WriteLine("Coffee...with milk.");
+                else if (Option == "Do you understand C?")
+                    Console.WriteLine("What's python?");
+                else if (Option == "Cats or dogs")
+                    Console.WriteLine("Cats");
+
+                Option = Console.ReadLine();
+                
+            }  
+
+        }
+    }
+}
