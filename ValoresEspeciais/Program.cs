035977bf06e6373f21e815564b0893c2ca95f0eb
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-28 11:27:01 +0000

    MinMax sbyte

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,9 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // MinAndMax Value of every type
+            Console.WriteLine($"Maximum value of sbyte: {sbyte.MaxValue}");
+            Console.WriteLine($"Minimum value of sbyte: {sbyte.MinValue}");
         }
     }
 }
