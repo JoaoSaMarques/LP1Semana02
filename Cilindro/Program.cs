ea71bd9864ae38590d079ed1d1037ef9c64ecca6
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-28 11:16:49 +0000

    Added surfaceArea calculation

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -20,7 +20,11 @@ namespace Cilindro
             //Convert to double
             float r = float.Parse(Second);
 
+            //Volume
             float Volume = MathF.PI * MathF.Pow(r, 2) * a;
+
+            //Surface
+            float surfaceArea = 2 * MathF.PI * r * (r + a);
 
             //print total volume
             Console.WriteLine($"Total volume: " + Volume);
