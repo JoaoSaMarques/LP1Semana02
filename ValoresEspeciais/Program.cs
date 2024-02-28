449a5250407ba1c0fced025acc8a63ab1e45e0d6
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-28 11:27:29 +0000

    MinMax byte

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -9,6 +9,9 @@ namespace ValoresEspeciais
             // MinAndMax Value of every type
             Console.WriteLine($"Maximum value of sbyte: {sbyte.MaxValue}");
             Console.WriteLine($"Minimum value of sbyte: {sbyte.MinValue}");
+
+            Console.WriteLine($"Maximum value of byte: {byte.MaxValue}");
+            Console.WriteLine($"Minimum value of byte: {byte.MinValue}");
         }
     }
 }
