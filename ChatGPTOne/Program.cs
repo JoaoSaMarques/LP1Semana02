b0f8ada6a28b3e5562be217795b839718d7bad16
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-28 12:06:10 +0000

    added exiter

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -14,6 +14,13 @@ namespace ChatGPTOne
                 // Colocar questão ao utilizador
                 Console.Write("Place your question (or type EXIT to quit): ");
                 question = Console.ReadLine();
+
+                // Check if the user wants to exit
+                if (question.ToUpper() == "EXIT")
+                {
+                    Console.WriteLine("Exiting the chatbot. Goodbye!");
+                    break;
+                }
             }
         }
     }
