7cd5c5bbe293b29d8c57f25167c57a37ca2a24cf
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-28 11:31:30 +0000

    MinMax infinity and nan

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,19 +6,31 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            // MinAndMax Value of every type
+            // MinAndMax Value of sbyte
             Console.WriteLine($"Maximum value of sbyte: {sbyte.MaxValue}");
             Console.WriteLine($"Minimum value of sbyte: {sbyte.MinValue}");
 
+            // MinAndMax Value of byte
             Console.WriteLine($"Maximum value of byte: {byte.MaxValue}");
             Console.WriteLine($"Minimum value of byte: {byte.MinValue}");
 
+            // MinAndMax Value of short
             Console.WriteLine($"Maximum value of short: {short.MaxValue}");
             Console.WriteLine($"Minimum value of short: {short.MinValue}");
 
+            // MinAndMax Value of ushort
             Console.WriteLine($"Maximum value of ushort: {ushort.MaxValue}");
             Console.WriteLine($"Minimum value of ushort: {ushort.MinValue}");
-            
+
+            // MinAndMax Value of infinity and float for nan INT
+            Console.WriteLine($"+ Infin for float: {float.PositiveInfinity}");
+            Console.WriteLine($"- Infin for float: {float.NegativeInfinity}");
+            Console.WriteLine($"NaN for float: {float.NaN}");
+
+            // MinAndMax Value of infinity and float for nan DOUBLE
+            Console.WriteLine($"+ Infin for double: {double.PositiveInfinity}");
+            Console.WriteLine($"- Infin for double: {double.NegativeInfinity}");
+            Console.WriteLine($"NaN for double: {double.NaN}");
         }
     }
 }
