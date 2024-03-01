a427fbe5ce2a79a13e1575817bde656f6bbc5ebb
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-01 13:38:13 +0000

    added comments to further decrease confusion

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -6,25 +6,31 @@ namespace MasterAndPlayer
     {
         static void Main(string[] args)
         {
+            //Ask gamemaster for number between 0 and 100
             Console.WriteLine("Please input a number between 0 and 100.");
             string playerInput1 = Console.ReadLine();
 
+            //Convert input to int
             int n = int.Parse(playerInput1);
 
+            //Loop
             if (n > 0 && n < 100)
             {
-                // Will increment number until it reaches 100
+                // Will increment number of blank lines until it reaches 100
                 for (int Blanks = 0; Blanks <= 100; Blanks++)
                 {
                     //Print 100 Blanks
                     Console.WriteLine(""); 
                 }
 
+                //Ask second user for number between 0 and 100
                 Console.WriteLine("Please input a number between 0 and 100.");
                 string playerInput2 = Console.ReadLine();
 
+                //Convert input to int
                 int i = int.Parse(playerInput2);
                 
+                //Loop
                 if (i > 0 && i < 100)
                 {
                     
