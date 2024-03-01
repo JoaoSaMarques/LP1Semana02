42cddeeafd09d6f7c191049cee4153de635b5a2e
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-01 13:12:28 +0000

    while loop and break if n is lower than 0 and higher than 100

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -6,10 +6,20 @@ namespace MasterAndPlayer
     {
         static void Main(string[] args)
         {
-            Console.Write("Please input a number between 1 and 100");
+            Console.Write("Please input a number between 0 and 100");
             string a = Console.ReadLine();
 
             int n = int.Parse(a);
+
+            while (n > 0 || n < 100)
+            {
+                if (n < 0 || n > 100)
+                {
+                    Console.Write("Error");
+                    break;
+                }
+            }
+
         }
     }
 }
