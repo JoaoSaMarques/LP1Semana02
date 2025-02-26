8052bafbf0fe298dec735174a1a702bec0f8c6a6
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-26 13:25:35 +0000

    add comments

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,12 +6,15 @@ namespace Cilindro
     {
         private static void Main(string[] args)
         {
+            // Prompt for height
             Console.WriteLine("What is the cylinder's height?");
             double a = Convert.ToDouble(Console.ReadLine());
 
+            // Prompt for radius
             Console.WriteLine("What is the cylinder's radius?");
             double r = Convert.ToDouble(Console.ReadLine());
 
+            // Calculate volume
             double V = Math.PI * Math.Pow(r, 2) * a;
 
         }
