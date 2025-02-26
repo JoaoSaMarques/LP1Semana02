2a87120063202e29c903c0c8e56437752cc213d4
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-26 13:13:08 +0000

    copy code from SmoothOperator

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -6,7 +6,25 @@ namespace VariousOperators
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Please input a non-negative integer number:");
+            string input = Console.ReadLine();
+            sbyte n;
+
+            if (sbyte.TryParse(input, out n))
+            {
+                // Perform decrement and increment
+                sbyte decrementResult = (sbyte)(n - 1);
+                sbyte incrementResult = (sbyte)(n + 1);
+
+                // Display results
+                Console.WriteLine($"Decrement result: {decrementResult}");
+                Console.WriteLine($"Increment result: {incrementResult}");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
         }
     }
 }
