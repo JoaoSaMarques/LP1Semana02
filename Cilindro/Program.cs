41863ecb6ba0fb74773b83d3c3d829c6511d53f9
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-26 13:22:14 +0000

    ask for cylinder height and convert to a

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,9 @@ namespace Cilindro
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("What is the cylinder's height?");
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
         }
     }
 }
