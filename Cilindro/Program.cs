4aeff50d961b18a52042997d1200092acd5e27de
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-26 13:25:06 +0000

    change int values to double

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,16 +7,13 @@ namespace Cilindro
         private static void Main(string[] args)
         {
             Console.WriteLine("What is the cylinder's height?");
-            string s1 = Console.ReadLine();
-            int a = int.Parse(s1);
+            double a = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("What is the cylinder's radius?");
-            string s2 = Console.ReadLine();
-            int r = int.Parse(s2);
+            double r = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("What is the cylinder's volume?");
-            string s3 = Console.ReadLine();
-            int V = int.Parse(s3);
+            double V = Math.PI * Math.Pow(r, 2) * a;
+
         }
     }
 }
