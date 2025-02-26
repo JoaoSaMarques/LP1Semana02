4cadf8604597eb18a4eae0b795fe4fd4ccc0ab18
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-26 13:30:29 +0000

    minmax short and ushort

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -9,6 +9,8 @@ namespace ValoresEspeciais
             // Print max and min values for various types
             Console.WriteLine($"byte: Max = {byte.MaxValue}, Min = {byte.MinValue}");
             Console.WriteLine($"sbyte: Max = {sbyte.MaxValue}, Min = {sbyte.MinValue}");
+            Console.WriteLine($"short: Max = {short.MaxValue}, Min = {short.MinValue}");
+            Console.WriteLine($"ushort: Max = {ushort.MaxValue}, Min = {ushort.MinValue}");
         }
     }
 }
