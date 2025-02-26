2f784e1ebb69d296403ce3b5f882d18bcb2b2895
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-26 13:06:39 +0000

    print decrease and add of n

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -9,6 +9,12 @@ namespace SmoothOperator
             Console.WriteLine("Please input a an integer number:");
             string s = Console.ReadLine();
             sbyte n = sbyte.Parse(s);
+
+            // Decremento de N
+            Console.WriteLine(n - 1);
+
+            // Incremento de N
+            Console.WriteLine(n + 1);
         }
     }
 }
