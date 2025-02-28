e400289c7248cf616d2816b800ff0776e23de84b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-28 14:22:38 +0000

    update comments

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main()
     {
-        // Define variables as int for both inside and outside loop
+        // Define variables as int for both inside and outside loop.
         int start, step;
 
         // Loop
         while (true)
         {
-            // Ask for start input
+            // Ask for start input.
             Console.Write("Start number: ");
             start = int.Parse(Console.ReadLine());
 
@@ -18,21 +18,21 @@ class Program
             Console.Write("Step number: ");
             step = int.Parse(Console.ReadLine());
 
-            // Specific requirements
+            // Specific requirements.
             if (start <= 1 || step < 1 || step > start)
             {
                 Console.WriteLine("Out-of-range start or step. Try again.");
                 continue;
             }
 
-            // Checks if step is higher than or equal to start
+            // Checks if step is higher than or equal to start.
             if (step >= start)
             {
                 Console.WriteLine("Start must be higher than step. Try again.");
                 continue;
             }
 
-            // Checks if start is divisible by step
+            // Checks if start is divisible by step.
             if (start % step != 0)
             {
                 Console.WriteLine("Start not divisible by step. Try again.");
@@ -43,7 +43,7 @@ class Program
             break; 
         }
 
-        // Countdown logic
+        // Countdown logic until i reaches 0.
         for (int i = start; i >= 0; i -= step)
         {
             Console.WriteLine(i);
