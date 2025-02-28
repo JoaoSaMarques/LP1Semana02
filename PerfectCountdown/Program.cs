0192ebaad98e2671e10612a1a042afd50a1e5cf8
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-28 14:07:04 +0000

    add into variables

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -7,8 +7,10 @@ namespace PerfectCountdown
         private static void Main(string[] args)
         {
             Console.Write("Start number: ");
+            int start = int.Parse(Console.ReadLine());
 
             Console.Write("Step number: ");
+            int step = int.Parse(Console.ReadLine());
         }
     }
 }
